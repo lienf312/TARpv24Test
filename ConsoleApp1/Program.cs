025ca10078ce7ab252fc11592864908ce56b1a14
Opilane TTHK <opilane@tthk.ee>
2025-02-03 15:42:18 +0200

    fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ConsoleApp1
+﻿namespace IfElseColor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string color = Console.ReadLine()?.ToLower();
+            if (color == "red")
+            {
+                Console.WriteLine("Sa valisid punase varvi");
+            }
+            else if (color == "blue")
+            {
+                Console.WriteLine("Sa valisid sinine varvi");
+            }
+            else if(color == "green")
+            {
+                Console.WriteLine("Sa valisid roheline varvi");
+            }
         }
     }
 }
