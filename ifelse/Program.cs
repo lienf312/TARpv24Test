7fa08d914a05830cb65bd406e0490d373536162c
Opilane TTHK <opilane@tthk.ee>
2025-02-03 15:40:33 +0200

    IfElseColor

--- ifelse/Program.cs
+++ ifelse/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Number control");
+
+            int number = 189;
+
+            if (number > 189)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Number is bigger");
+
+                //if ja ele neitimine
+                if (number > 190 && number < 200)
+                {
+                    Console.WriteLine("Number is between 190 to 200")
+                }
+                else
+                {
+                    Console.WriteLine("Number is bigger than 200");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Number is smaller");
+
+            }
         }
     }
 }
+
